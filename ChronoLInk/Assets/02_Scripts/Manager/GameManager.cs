2c2ea53aadc5641f9cb7dc91ea08f3a5f1c361e0
Woong1016 <yol7122@naver.com>
2025-10-30 08:58:35 +0900

    [Fix]시간개변 기능 오류 해결
    (원인 빈 오브젝트 안에 Tiem object 스크립트가 들어 있지 않았고 Tiem object 들어있는 오브젝트가 먼저 감지가 되지 않아서 )

--- ChronoLInk/Assets/02_Scripts/Manager/GameManager.cs
+++ ChronoLInk/Assets/02_Scripts/Manager/GameManager.cs
@@ -4,14 +4,6 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-using UnityEngine;
-using UnityEngine.SceneManagement; // 씬 관리를 위해 추가
-using Photon.Pun;
-using Photon.Voice.Unity; // 보이스 관리를 위해 추가
-using System.Collections.Generic;
-
-
-
 public class GameManager : MonoBehaviourPunCallbacks
 {
 
@@ -21,20 +13,20 @@ public class GameManager : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject pastPlayerPrefab;
     [SerializeField] private GameObject futurePlayerPrefab;
 
-    // 방장만 사용하는 변수들
-    private List<int> readyPlayers = new List<int>();
+    // 방장만 사용하는 변수들
+    private List<int> readyPlayers = new List<int>();
     private bool isSpawningStarted = false;
 
     private Dictionary<string, TimeObject> futureTimeObjects = new Dictionary<string, TimeObject>();
 
-    // 보이스 연결을 제어하기 위한 변수
-    private VoiceConnection voiceConnection;
+    // 보이스 연결을 제어하기 위한 변수
+    private VoiceConnection voiceConnection;
 
-    #region 유니티 생명주기 및 싱글톤
-    void Awake()
+    #region 유니티 생명주기 및 싱글톤
+    void Awake()
     {
-        // 싱글톤 인스턴스 설정
-        if (Instance == null)
+        // 싱글톤 인스턴스 설정
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -53,27 +45,27 @@ public class GameManager : MonoBehaviourPunCallbacks
         photonView.RPC("SignalPlayerIsReady", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.ActorNumber);
 
     }
-    #endregion
+    #endregion
 
-    #region 방 나가기 및 보이스 연결 해제 (새로운 핵심 로직)
-    public void LeaveGameAndReturnToLobby()
+    #region 방 나가기 및 보이스 연결 해제 (새로운 핵심 로직)
+    public void LeaveGameAndReturnToLobby()
     {
-        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveRoom();
     }
 
-    public override void OnLeftRoom()
+    public override void OnLeftRoom()
     {
-        if (voiceConnection != null && voiceConnection.Client != null && voiceConnection.Client.IsConnected)
+        if (voiceConnection != null && voiceConnection.Client != null && voiceConnection.Client.IsConnected)
         {
             voiceConnection.Client.Disconnect();
         }
 
-        SceneManager.LoadScene("LobbyScene");
+        SceneManager.LoadScene("LobbyScene");
     }
-    #endregion
+    #endregion
 
-    #region 기존 플레이어 스폰 로직
-    [PunRPC]
+    #region 기존 플레이어 스폰 로직
+    [PunRPC]
     void SignalPlayerIsReady(int actorNumber)
     {
         if (!PhotonNetwork.IsMasterClient) return;
@@ -100,54 +92,63 @@ public class GameManager : MonoBehaviourPunCallbacks
             PhotonNetwork.Instantiate(prefabToSpawn.name, spawnPosition, Quaternion.identity);
         }
     }
-    #endregion
+    #endregion
+
+    // --- 시간 개변 로직 ---
+
     public void RegisterTimeObject(string id, TimeObject obj)
     {
         if (!futureTimeObjects.ContainsKey(id))
         {
-           
+
             futureTimeObjects.Add(id, obj);
             Debug.Log($"GameManager: 미래 오브젝트 '{id}' (InstanceID: {obj.GetInstanceID()}) 등록 완료.");
         }
-        else  
+        else
         {
-
             futureTimeObjects[id] = obj;
             Debug.LogWarning($"GameManager: 미래 오브젝트 ID '{id}'가 이미 존재하여 업데이트합니다.");
         }
     }
 
-    public void SyncTimeObject(string id, Vector3 newLocalPosition)
+    public void SyncTimeObject(string id, Vector3 pastLocalPosition) // 💡 변수명 변경 (newLocalPosition -> pastLocalPosition)
     {
-        Debug.Log($"GameManager.SyncTimeObject received request: ID '{id}', Pos: {newLocalPosition}. Checking PhotonView...");
+        Debug.Log($"GameManager.SyncTimeObject received request: ID '{id}', PastPos: {pastLocalPosition}. Checking PhotonView...");
 
-        PhotonView pv = GetComponent<PhotonView>();  
+        PhotonView pv = GetComponent<PhotonView>();
 
-        if (pv != null)  
+        if (pv != null)
         {
             Debug.Log("GameManager PhotonView found. Attempting to send RPC...");
-            pv.RPC("RPC_UpdateFutureObjectPosition", RpcTarget.All, id, newLocalPosition);
+            // 💡 과거의 위치(pastLocalPosition)를 그대로 RPC로 전송
+            pv.RPC("RPC_UpdateFutureObjectPosition", RpcTarget.All, id, pastLocalPosition);
         }
         else
         {
-            
             Debug.LogError("GameManager is MISSING PhotonView component! Cannot send SyncTimeObject RPC.");
         }
     }
 
+    // 💡 [핵심 수정] 이 함수가 수정되었습니다.
     [PunRPC]
-    void RPC_UpdateFutureObjectPosition(string id, Vector3 newLocalPosition)
+    void RPC_UpdateFutureObjectPosition(string id, Vector3 pastLocalPosition) // 💡 변수명 변경
     {
-
-        Debug.Log($"RPC_UpdateFutureObjectPosition EXECUTED on client: {PhotonNetwork.LocalPlayer.NickName}. ID: '{id}', Pos: {newLocalPosition}");
+        Debug.Log($"RPC_UpdateFutureObjectPosition EXECUTED on client: {PhotonNetwork.LocalPlayer.NickName}. ID: '{id}', PastPos: {pastLocalPosition}");
 
         TimeObject futureObj;
         if (futureTimeObjects.TryGetValue(id, out futureObj))
         {
-            if (futureObj != null)  
+            if (futureObj != null)
             {
-                // 로컬 위치 업데이트
-                futureObj.transform.localPosition = newLocalPosition;
+                // 💡 [수정] 과거 위치에 +40 오프셋을 더해 '미래 위치'를 계산합니다.
+                Vector3 newFutureLocalPosition = new Vector3(
+                    pastLocalPosition.x + 40f, // X축에 40 추가
+                    pastLocalPosition.y,
+                    pastLocalPosition.z
+                );
+
+                // 💡 [수정] 계산된 '미래 위치'로 설정합니다.
+                futureObj.transform.localPosition = newFutureLocalPosition;
             }
             else // 파괴된 오브젝트 목록에서 제거
             {
@@ -155,5 +156,4 @@ public class GameManager : MonoBehaviourPunCallbacks
             }
         }
     }
-
 }
